cc642dfb41445da986f7f2ea4634ef2fe78e5d1e
EngCamo <learn3038it@gmail.com>
2023-12-24 23:07:36 +0800

    chnge title

--- OOP_Final_Project/Source/MainApp.cpp
+++ OOP_Final_Project/Source/MainApp.cpp
@@ -29,12 +29,7 @@ participants: Fahad , Abdikarin , marian & abdullahi
 int main(){
 
    // current date and time on the current system
-   time_t now = time(0);
 
-   // convert now to string form
-   char* date_time = ctime(&now);
-
-   cout << "The current date and time is: " << date_time << endl;
 
     return 0;
 }
