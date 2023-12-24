318c5d4b6e91ad338ffeef582bcf02145ba35784
EngCamo <learn3038it@gmail.com>
2023-12-24 16:19:43 +0800

    added OOP_finalProject

---
+++ OOP_Final_Project/Source/Linklist(LMS).hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "LMS.hpp"
+
+/*
+    *methods needed in this **
+    Search()
+    Display()
+    Update()
+    Delete()
+    Add()
+    generateReport()
+    
+*/
---
+++ OOP_Final_Project/Source/MainApp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "LMS.hpp"
+
+
+using namespace std;
+
+/*
+participants: Fahad , Abdikarin , marian & abdullahi
+ ______________________________________________________________________________________________________
+|1- Demonstrate the following methods with records of objects: create and insert new object,           |
+|remove existing object, search object, update object, and display object(s). (A3, 16 marks)           |
+|                                                                                                      |
+|2- Be logical in the design of the solution – implement at least 3 classes including main() class,    |
+|apply inheritance, create linked list of objects or array of objects, input validation (C4, 10        |
+|marks)                                                                                                |
+|                                                                                                      |
+|3- Implement tasks to manipulate records in producing meaningful output – (e.g. apply                 |
+|searching and/or sorting to generate reports, perform calculation and others) (P4, 14 marks)          |
+|                                                                                                      |
+|4- Submit the project report on week 13. Presentation will be scheduled in week 14. The cover         |
+|page shall contain UTHM logo, project title, names and matric no of all project members.              |
+|______________________________________________________________________________________________________|
+
+                    the main driver needs to be able to read write to a .txt file
+*/
+int main(){
+
+
+
+
+    return 0;
+}
